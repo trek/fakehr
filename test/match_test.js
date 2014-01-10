1c6cd2d5296cee8d0d88cf3aaa8786d808e9fd25
Kamal Mahyuddin <kamal@viki.com>
2014-01-09 17:50:39 -0800

    support matching on request body for non-GET/HEAD requests

--- test/match_test.js
+++ test/match_test.js
@@ -31,3 +31,22 @@ test("only returns first object found", function(){
 
   equal(fakehr.match('get', '/a/path'), xhr);
 });
+
+test("matches any request by HTTP method, url, and data", function(){
+  var xhr = new XMLHttpRequest();
+  xhr.open('post', '/a/path');
+  xhr.send({ foo: 'bar' });
+  equal(fakehr.match('post', '/a/path', { foo: 'bar' }, 1), xhr);
+});
+
+test("differentiates a request for the same path by data", function(){
+  var xhr = new XMLHttpRequest();
+  var xhr2 = new XMLHttpRequest();
+  xhr.open('post', '/a/path', { foo: 'bar' });
+  xhr.send({ foo: 'bar' });
+  xhr2.open('post', '/a/path', { bar: 'baz' });
+  xhr2.send({ bar: 'baz' });
+
+  equal(fakehr.match('post', '/a/path', { foo: 'bar' }), xhr);
+  equal(fakehr.match('post', '/a/path', { bar: 'baz' }), xhr2);
+});
